bd36f85ad451a7535c871cfa8bdd16ec38497339
Jie Feng <jiefeng.hopkins@gmail.com>
2015-07-11 22:21:26 +0800

    updated jssha

--- config.example.js
+++ config.example.js
@@ -1,13 +1,13 @@
 'use strict';
 
 exports.port = process.env.PORT || 3000;
-exports.companyName = 'Beijing Dream Castle Culture Co., Ltd';
-exports.projectName = '梦之城微信控制中心';
-exports.systemEmail = 'xxx@a-li.com.cn';
+exports.companyName = 'Pairyo Co., Ltd';
+exports.projectName = 'Pairyo';
+exports.systemEmail = 'pairyo@pairyo.com';
 exports.cryptoKey = 'k4yb0ardc4x';
 
 exports.weixin = {
-  localSource: 'dreamcastle',
+  localSource: 'pairyo',
   AppId: 'wx9999999999',
   AppSecret: 'ad73709c6e0815c999999999999'
 };
